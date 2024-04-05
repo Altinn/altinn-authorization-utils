a97773479b810112faa5a2e7ad2a12e1359c4544
Aleksander Heintz <alxandr@alxandr.me>
2024-04-05 23:49:06 +0200

    fix: add more doc comments

--- src/Altinn.Urn/src/Altinn.Urn/UrnAttribute.cs
+++ src/Altinn.Urn/src/Altinn.Urn/UrnAttribute.cs
@@ -1,5 +1,8 @@
 ﻿namespace Altinn.Urn;
 
+/// <summary>
+/// Marker attribute used by the source generator to generate a URN types.
+/// </summary>
 [AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = false)]
 public sealed class UrnAttribute : Attribute
 {
--- src/Altinn.Urn/src/Altinn.Urn/UrnTypeAttribute.cs
+++ src/Altinn.Urn/src/Altinn.Urn/UrnTypeAttribute.cs
@@ -1,5 +1,9 @@
 ﻿namespace Altinn.Urn;
 
+/// <summary>
+/// Marker attribute used by the source generator to generate URN types.
+/// Applied to "type" methods to add valid URN prefixes.
+/// </summary>
 [AttributeUsage(AttributeTargets.Method, Inherited = false, AllowMultiple = true)]
 public sealed class UrnTypeAttribute : Attribute
 {
