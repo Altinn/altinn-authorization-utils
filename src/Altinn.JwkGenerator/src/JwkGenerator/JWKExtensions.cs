7d49235753c8a8530ae856ab84aaba0d4dc4ef85
Aleksander Heintz <alxandr@alxandr.me>
2024-06-21 15:52:16 +0200

    chore: big refactor

---
+++ src/Altinn.JwkGenerator/src/JwkGenerator/JWKExtensions.cs
@@ -0,0 +1,39 @@
+﻿using CommunityToolkit.Diagnostics;
+using Microsoft.IdentityModel.Tokens;
+using System.Diagnostics.CodeAnalysis;
+using System.Security.Cryptography;
+
+namespace Altinn.Authorization.JwkGenerator;
+
+[ExcludeFromCodeCoverage]
+internal static class JWKExtensions
+{
+    public static string ToJwkString(this JWKAlgorithm alg)
+        => alg switch
+        {
+            JWKAlgorithm.RS256 => SecurityAlgorithms.RsaSha256,
+            JWKAlgorithm.RS384 => SecurityAlgorithms.RsaSha384,
+            JWKAlgorithm.RS512 => SecurityAlgorithms.RsaSha512,
+            JWKAlgorithm.ES256 => SecurityAlgorithms.EcdsaSha256,
+            JWKAlgorithm.ES384 => SecurityAlgorithms.EcdsaSha384,
+            JWKAlgorithm.ES512 => SecurityAlgorithms.EcdsaSha512,
+            _ => ThrowHelper.ThrowArgumentException<string>(nameof(alg), "Unknown JWK Algorithm"),
+        };
+
+    public static string ToJwkString(this JWKUse use)
+        => use switch
+        {
+            JWKUse.Sig => JsonWebKeyUseNames.Sig,
+            JWKUse.Enc => JsonWebKeyUseNames.Enc,
+            _ => ThrowHelper.ThrowArgumentException<string>(nameof(use), "Unknown JWK Use"),
+        };
+
+    public static ECCurve ToECDsaCurve(this JWKAlgorithm alg)
+        => alg switch
+        {
+            JWKAlgorithm.ES256 => ECCurve.NamedCurves.nistP256,
+            JWKAlgorithm.ES384 => ECCurve.NamedCurves.nistP384,
+            JWKAlgorithm.ES512 => ECCurve.NamedCurves.nistP521,
+            _ => ThrowHelper.ThrowArgumentException<ECCurve>(nameof(alg), "Unknown JWK Algorithm"),
+        };
+}
---
+++ src/Altinn.JwkGenerator/src/JwkGenerator/JWKUse.cs
@@ -0,0 +1,17 @@
+﻿namespace Altinn.Authorization.JwkGenerator;
+
+/// <summary>
+/// JWK use.
+/// </summary>
+public enum JWKUse
+{
+    /// <summary>
+    /// Key is used for signing.
+    /// </summary>
+    Sig,
+
+    /// <summary>
+    /// Key is used for encryption.
+    /// </summary>
+    Enc,
+}
