fbe3bac984474d230464d6297356de39abe1d2f9
Aleksander Heintz <alxandr@alxandr.me>
2024-12-17 14:47:37 +0100

    test: add tests

--- src/Altinn.Authorization.ProblemDetails/src/ProblemDetails.Abstractions/ProblemInstanceException.cs
+++ src/Altinn.Authorization.ProblemDetails/src/ProblemDetails.Abstractions/ProblemInstanceException.cs
@@ -1,7 +1,7 @@
 ﻿namespace Altinn.Authorization.ProblemDetails;
 
 /// <summary>
-/// An exception that represents a <see cref="ProblemInstance"/>.
+/// An exception that represents a <see cref="Problem"/>.
 /// </summary>
 public class ProblemInstanceException
     : InvalidOperationException
@@ -9,15 +9,15 @@ public class ProblemInstanceException
     /// <summary>
     /// Initializes a new instance of the <see cref="ProblemInstanceException"/> class.
     /// </summary>
-    /// <param name="problemInstance">The <see cref="ProblemInstance"/>.</param>
+    /// <param name="problemInstance">The <see cref="Problem"/>.</param>
     public ProblemInstanceException(ProblemInstance problemInstance)
         : base(problemInstance.Detail)
     {
-        ProblemInstance = problemInstance;
+        Problem = problemInstance;
     }
 
     /// <summary>
-    /// Gets the <see cref="ProblemInstance"/>.
+    /// Gets the <see cref="Problem"/>.
     /// </summary>
-    public ProblemInstance ProblemInstance { get; }
+    public ProblemInstance Problem { get; }
 }
