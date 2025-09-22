4fec25fd53835d227c0d01b74666fcdb145c0aee
Aleksander Heintz <alxandr@alxandr.me>
2025-09-22 20:06:43 +0200

    feat: create AsyncOnlyDelegatingHandler

---
+++ src/Altinn.Authorization.ServiceDefaults/src/ServiceDefaults.HttpClient/AsyncOnlyDelegatingHandler.cs
@@ -0,0 +1,19 @@
+﻿using CommunityToolkit.Diagnostics;
+
+namespace Altinn.Authorization.ServiceDefaults.HttpClient;
+
+/// <summary>
+/// Provides a base class for HTTP message handlers that support only asynchronous operations.
+/// </summary>
+/// <remarks>This class disables synchronous HTTP request processing by overriding the synchronous Send method to
+/// throw a NotSupportedException. Derived handlers must implement asynchronous logic using SendAsync. Use this class
+/// when creating handlers that should not support synchronous execution.</remarks>
+public abstract class AsyncOnlyDelegatingHandler
+    : DelegatingHandler
+{
+    /// <inheritdoc/>
+    protected sealed override HttpResponseMessage Send(HttpRequestMessage request, CancellationToken cancellationToken)
+    {
+        return ThrowHelper.ThrowNotSupportedException<HttpResponseMessage>("Synchronous Send is not supported. Use SendAsync instead.");
+    }
+}
---
+++ src/Altinn.Authorization.ServiceDefaults/test/ServiceDefaults.Tests/AsyncOnlyDelegatingHandlerTests.cs
@@ -0,0 +1,25 @@
+﻿using Altinn.Authorization.ServiceDefaults.HttpClient;
+
+namespace Altinn.Authorization.ServiceDefaults.Tests;
+
+public class AsyncOnlyDelegatingHandlerTests
+{
+    [Fact]
+    public void Send_ThrowsNotSupportedException()
+    {
+        var handler = new TestHandler();
+        var request = new HttpRequestMessage(HttpMethod.Get, "https://example.com");
+        using var client = new System.Net.Http.HttpClient(handler);
+        var exception = Should.Throw<NotSupportedException>(() => client.Send(request, CancellationToken.None));
+    }
+
+    public class TestHandler 
+        : AsyncOnlyDelegatingHandler
+    {
+        protected override Task<HttpResponseMessage> SendAsync(HttpRequestMessage request, CancellationToken cancellationToken)
+        {
+            var response = new HttpResponseMessage(System.Net.HttpStatusCode.OK);
+            return Task.FromResult(response);
+        }
+    }
+}
