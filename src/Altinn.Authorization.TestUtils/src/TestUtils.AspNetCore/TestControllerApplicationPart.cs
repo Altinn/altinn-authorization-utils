fa86525f1648afa684a4778c49b421435dc3df8b
Aleksander Heintz <alxandr@alxandr.me>
2025-10-18 12:14:56 +0200

    feat: aspnet testing utils (#389)

---
+++ src/Altinn.Authorization.TestUtils/src/TestUtils.AspNetCore/TestControllerApplicationPart.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.ApplicationParts;
+using System.Reflection;
+
+namespace Altinn.Authorization.TestUtils.AspNetCore;
+
+internal class TestControllerApplicationPart(TypeInfo type)
+    : ApplicationPart
+{
+    public override string Name => $"{nameof(TestControllerApplicationPart)}:{type.FullName}";
+
+    public TypeInfo Type => type;
+}
