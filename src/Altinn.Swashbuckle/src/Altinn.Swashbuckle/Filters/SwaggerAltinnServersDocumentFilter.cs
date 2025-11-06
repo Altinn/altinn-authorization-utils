cc8c645211aa20b7ed75f77b16b1d2f688c7349a
Aleksander Heintz <alxandr@alxandr.me>
2025-11-06 17:06:38 +0100

    feat: add Altinn servers to OpenAPI documents (#415)

---
+++ src/Altinn.Swashbuckle/src/Altinn.Swashbuckle/Filters/SwaggerAltinnServersDocumentFilter.cs
@@ -0,0 +1,97 @@
+﻿using Altinn.Swashbuckle.Servers;
+using Microsoft.AspNetCore.Hosting.Server;
+using Microsoft.AspNetCore.Hosting.Server.Features;
+using Microsoft.Extensions.Options;
+using Microsoft.OpenApi.Models;
+using Swashbuckle.AspNetCore.SwaggerGen;
+using System.Collections.Immutable;
+using System.Diagnostics.CodeAnalysis;
+
+namespace Altinn.Swashbuckle.Filters;
+
+[ExcludeFromCodeCoverage]
+internal sealed class SwaggerAltinnServersDocumentFilter
+    : IDocumentFilter
+{
+    private readonly IOptionsMonitor<AltinnServerOptions> _options;
+    private readonly ImmutableArray<string> _localAddresses;
+
+    public SwaggerAltinnServersDocumentFilter(IOptionsMonitor<AltinnServerOptions> options, IServer server)
+    {
+        _options = options;
+        _localAddresses = server.Features.Get<IServerAddressesFeature>()?.Addresses?.ToImmutableArray() ?? [];
+    }
+
+    public void Apply(OpenApiDocument swaggerDoc, DocumentFilterContext context)
+    {
+        var options = _options.Get(context.DocumentName);
+        var defaultOptions = _options.CurrentValue;
+        var suffix = options.EnvironmentServerPathSuffix.HasValue ? options.EnvironmentServerPathSuffix : defaultOptions.EnvironmentServerPathSuffix;
+
+        if (options.IncludeLocalhostServer ?? defaultOptions.IncludeLocalhostServer ?? true) 
+        {
+            swaggerDoc.Servers.Add(new OpenApiServer
+            {
+                Description = "Local",
+                Url = "{address}/",
+                Variables = new Dictionary<string, OpenApiServerVariable>
+                {
+                    {
+                        "address",
+                        new OpenApiServerVariable
+                        {
+                            Default = _localAddresses[0],
+                            Enum = [.. _localAddresses],
+                        }
+                    }
+                }
+            });
+        }
+
+        if (options.IncludeAcceptanceTestServers ?? defaultOptions.IncludeAcceptanceTestServers ?? true)
+        {
+            swaggerDoc.Servers.Add(new OpenApiServer
+            {
+                Description = "Acceptance Test",
+                Url = string.Concat("https://platform.{environment}.altinn.cloud/", suffix),
+                Variables = new Dictionary<string, OpenApiServerVariable> {
+                    { 
+                        "environment", 
+                        new OpenApiServerVariable 
+                        {
+                            Default = "at22",
+                            Enum = ["at22", "at23", "at24"],
+                        }
+                    },
+                },
+            });
+        }
+
+        if (options.IncludePerformanceTestServer ?? defaultOptions.IncludePerformanceTestServer ?? true)
+        {
+            swaggerDoc.Servers.Add(new OpenApiServer
+            {
+                Description = "Performance Test",
+                Url = string.Concat("https://platform.yt01.altinn.cloud/", suffix),
+            });
+        }
+
+        if (options.IncludeIntegrationTestServer ?? defaultOptions.IncludeIntegrationTestServer ?? true)
+        {
+            swaggerDoc.Servers.Add(new OpenApiServer
+            {
+                Description = "Integration Test",
+                Url = string.Concat("https://platform.tt02.altinn.no/", suffix),
+            });
+        }
+
+        if (options.IncludeProductionServers ?? defaultOptions.IncludeProductionServers ?? true)
+        {
+            swaggerDoc.Servers.Add(new OpenApiServer
+            {
+                Description = "Production",
+                Url = string.Concat("https://platform.altinn.no/", suffix),
+            });
+        }
+    }
+}
---
+++ src/Altinn.Swashbuckle/src/Altinn.Swashbuckle/Servers/AltinnServerOptions.cs
@@ -0,0 +1,39 @@
+﻿using Microsoft.AspNetCore.Http;
+
+namespace Altinn.Swashbuckle.Servers;
+
+/// <summary>
+/// Provides options for configuring Altinn servers in Swagger documentation.
+/// </summary>
+public sealed class AltinnServerOptions
+{
+    /// <summary>
+    /// Gets or sets a value indicating whether the localhost server should be included in OpenAPI document.
+    /// </summary>
+    public bool? IncludeLocalhostServer { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether acceptance test servers (AT) should be included in OpenAPI document.
+    /// </summary>
+    public bool? IncludeAcceptanceTestServers { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether performance test servers (YT) should be included in OpenAPI document.
+    /// </summary>
+    public bool? IncludePerformanceTestServer { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the integration test server (TT) should be included in OpenAPI document.
+    /// </summary>
+    public bool? IncludeIntegrationTestServer { get; set; }
+
+    /// <summary>
+    /// Gets or sets a value indicating whether the production server should be included in OpenAPI document.
+    /// </summary>
+    public bool? IncludeProductionServers { get; set; }
+
+    /// <summary>
+    /// Gets or sets the path suffix to append to the server URL for all non-localhost servers.
+    /// </summary>
+    public PathString EnvironmentServerPathSuffix { get; set; }
+}
