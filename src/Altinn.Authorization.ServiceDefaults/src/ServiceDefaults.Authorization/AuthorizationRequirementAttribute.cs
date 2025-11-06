6586e4db5ee7844c73800a3242a9c75e3f521f64
Aleksander Heintz <alxandr@alxandr.me>
2025-11-06 15:09:38 +0100

    fix: `AuthorizationRequirementAttribute` should not implement `IAuthorizationData`. (#414)

--- src/Altinn.Authorization.ServiceDefaults/src/ServiceDefaults.Authorization/AuthorizationRequirementAttribute.cs
+++ src/Altinn.Authorization.ServiceDefaults/src/ServiceDefaults.Authorization/AuthorizationRequirementAttribute.cs
@@ -1,12 +1,15 @@
-using Microsoft.AspNetCore.Authorization;
+﻿using Microsoft.AspNetCore.Authorization;
+using System.Diagnostics;
 
 namespace Altinn.Authorization.ServiceDefaults.Authorization;
 
 /// <summary>
 /// Helper base class for authorization-requirement attributes.
 /// </summary>
+[DebuggerDisplay("{ToString(),nq}")]
+[AttributeUsage(AttributeTargets.Class | AttributeTargets.Method, AllowMultiple = true, Inherited = true)]
 public abstract class AuthorizationRequirementAttribute
-    : AuthorizeAttribute
+    : Attribute
     , IAuthorizationRequirement
     , IAuthorizationRequirementData
 {
