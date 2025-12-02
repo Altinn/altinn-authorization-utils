0d2fd21cef3389d2e2ff3827bbe9898277380026
Aleksander Heintz <alxandr@alxandr.me>
2025-12-02 12:20:40 +0100

    feat: trusted proxies

--- src/Altinn.Authorization.ServiceDefaults/src/ServiceDefaults/Options/ConfigureAltinnClusterInfo.cs
+++ src/Altinn.Authorization.ServiceDefaults/src/ServiceDefaults/Options/ConfigureAltinnClusterInfo.cs
@@ -1,23 +1,29 @@
 ﻿using Altinn.Authorization.ServiceDefaults.Utils;
 using CommunityToolkit.Diagnostics;
 using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Logging;
 using Microsoft.Extensions.Options;
 
 namespace Altinn.Authorization.ServiceDefaults.Options;
 
-internal class ConfigureAltinnClusterInfo
+internal partial class ConfigureAltinnClusterInfo
     : IConfigureOptions<AltinnClusterInfo>
 {
     private readonly IConfiguration _config;
+    private readonly ILogger<ConfigureAltinnClusterInfo> _logger;
 
-    public ConfigureAltinnClusterInfo(IConfiguration config)
+    public ConfigureAltinnClusterInfo(
+        IConfiguration config,
+        ILogger<ConfigureAltinnClusterInfo> logger)
     {
         _config = config;
+        _logger = logger;
     }
 
     public void Configure(AltinnClusterInfo options)
     {
         ConfigureClusterNetwork(options);
+        ConfigureTrustedProxies(options);
     }
 
     private void ConfigureClusterNetwork(AltinnClusterInfo options)
@@ -33,6 +39,42 @@ internal class ConfigureAltinnClusterInfo
             ThrowHelper.ThrowFormatException("Invalid CIDR format for Altinn:ClusterInfo:ClusterNetwork");
         }
 
+#pragma warning disable CS0618 // Type or member is obsolete
         options.ClusterNetwork = ipNetwork;
+#pragma warning restore CS0618 // Type or member is obsolete
+
+        Log.ClusterNetworkDeprecated(_logger);
+    }
+
+    private void ConfigureTrustedProxies(AltinnClusterInfo options)
+    {
+        var proxies = _config.GetValue<string>("Altinn:ClusterInfo:TrustedProxies");
+        if (string.IsNullOrEmpty(proxies))
+        {
+            return;
+        }
+
+        var span = proxies.AsSpan();
+        foreach (var range in span.Split(','))
+        {
+            var segment = span[range].Trim();
+            if (segment.IsEmpty)
+            {
+                continue;
+            }
+
+            if (!IPNetworkUtils.TryParseIPNetwork(segment, out var ipNetwork, out _))
+            {
+                ThrowHelper.ThrowFormatException($"Invalid CIDR format in Altinn:ClusterInfo:TrustedProxies. Got: {segment}");
+            }
+
+            options.TrustedProxies.Add(ipNetwork);
+        }
+    }
+
+    private static partial class Log
+    {
+        [LoggerMessage(0, LogLevel.Warning, "'Altinn:ClusterInfo:ClusterNetwork' is deprecated, use 'Altinn:ClusterInfo:TrustedProxies' instead")]
+        public static partial void ClusterNetworkDeprecated(ILogger logger);
     }
 }
