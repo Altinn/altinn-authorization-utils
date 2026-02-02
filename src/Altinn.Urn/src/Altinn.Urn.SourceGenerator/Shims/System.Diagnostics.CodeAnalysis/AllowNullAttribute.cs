6375e630fa098ae3ea242d48301c9bd54822ddb9
Aleksander Heintz <alxandr@alxandr.me>
2026-02-02 10:01:49 +0100

    chore: migrate to polyfill

--- src/Altinn.Urn/src/Altinn.Urn.SourceGenerator/Shims/System.Diagnostics.CodeAnalysis/AllowNullAttribute.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace System.Diagnostics.CodeAnalysis;
-
-/// <summary>Specifies that null is allowed as an input even if the corresponding type disallows it.</summary>
-[AttributeUsage(AttributeTargets.Field | AttributeTargets.Parameter | AttributeTargets.Property, Inherited = false)]
-[ExcludeFromCodeCoverage]
-public sealed class AllowNullAttribute : Attribute
-{ 
-}
