64513f2ac8b51ad4193f09126707bb50540c6f19
Aleksander Heintz <alxandr@alxandr.me>
2026-02-11 12:49:13 +0100

    feat: add otel-sampling

---
+++ src/Altinn.Authorization.ServiceDefaults/src/ServiceDefaults/AltinnTelemetryOptions.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Altinn.Authorization.ServiceDefaults;
+
+/// <summary>
+/// Represents configuration options for telemetry collection in Altinn applications.
+/// </summary>
+public class AltinnTelemetryOptions
+{
+    /// <summary>
+    /// Gets or sets the proportion of items to include during sampling.
+    /// </summary>
+    /// <remarks>The value must be between 0.0 and 1.0, inclusive. A value of 0.0 means no items are sampled;
+    /// a value of 1.0 means all items are included.</remarks>
+    [Range(0.0, 1.0)]
+    public double SamplingRatio { get; set; } = 1.0;
+}
