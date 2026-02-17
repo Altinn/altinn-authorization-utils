944058f4039322261f5dde7e317b261922106f0a
Aleksander Heintz <alxandr@alxandr.me>
2026-02-17 14:48:44 +0100

    fix: allow activity-collector to not collect everything

--- src/Altinn.Authorization.TestUtils/src/TestUtils/ActivityCollector.cs
+++ src/Altinn.Authorization.TestUtils/src/TestUtils/ActivityCollector.cs
@@ -11,8 +11,27 @@ namespace Altinn.Authorization.TestUtils;
 public sealed class ActivityCollector
     : ICollection<Activity>
 {
+    private string? _rootSourceName;
     private ImmutableList<Activity> _activities = [];
-    
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ActivityCollector"/> class with an optional root source name filter.
+    /// </summary>
+    /// <param name="rootSourceName">The name of the root source from which to collect activities. This parameter can be null to indicate that no
+    /// specific root source is targeted.</param>
+    public ActivityCollector(string? rootSourceName)
+    {
+        _rootSourceName = rootSourceName;
+    }
+
+    /// <summary>
+    /// Initializes a new instance of the <see cref="ActivityCollector"/> class.
+    /// </summary>
+    public ActivityCollector()
+        : this(rootSourceName: null)
+    {
+    }
+
     /// <inheritdoc/>
     public int Count 
         => Volatile.Read(ref _activities).Count;
@@ -22,7 +41,37 @@ public sealed class ActivityCollector
 
     /// <inheritdoc/>
     void ICollection<Activity>.Add(Activity item)
-        => ImmutableInterlocked.Update(ref _activities, static (list, item) => list.Add(item), item);
+    {
+        if (_rootSourceName is { } name && !IsActivityFromRootSource(item, name))
+        {
+            return;
+        }
+
+        ImmutableInterlocked.Update(ref _activities, static (list, item) => list.Add(item), item);
+
+        static bool IsActivityFromRootSource(Activity activity, string rootSourceName)
+        {
+            if (activity.Source.Name == rootSourceName)
+            {
+                // we do not keep the actual root activity
+                return false;
+            }
+
+            var current = activity;
+
+            while (current is not null)
+            {
+                if (current.Source.Name == rootSourceName)
+                {
+                    return true;
+                }
+
+                current = current.Parent;
+            }
+
+            return false;
+        }
+    }
 
     /// <inheritdoc/>
     void ICollection<Activity>.Clear()
