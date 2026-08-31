351792aa92ed05f5c73c7d5e7e1fe3d36e051b7d
Aleksander Heintz <alxandr@alxandr.me>
2026-08-31 12:37:12 +0200

    chore: seal all stream overrides

--- src/pkgs/Altinn.Authorization.RepoCtl/src/RepoCtl.Model/Utils/DelegatingStream.cs
+++ src/pkgs/Altinn.Authorization.RepoCtl/src/RepoCtl.Model/Utils/DelegatingStream.cs
@@ -23,39 +23,39 @@ public abstract class DelegatingStream
     }
 
     /// <inheritdoc/>
-    public override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+    public sealed override IAsyncResult BeginRead(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
         => _baseStream.BeginRead(buffer, offset, count, callback, state);
 
     /// <inheritdoc/>
-    public override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
+    public sealed override IAsyncResult BeginWrite(byte[] buffer, int offset, int count, AsyncCallback? callback, object? state)
         => _baseStream.BeginWrite(buffer, offset, count, callback, state);
 
     /// <inheritdoc/>
-    public override bool CanRead
+    public sealed override bool CanRead
         => _baseStream.CanRead;
 
     /// <inheritdoc/>
-    public override bool CanWrite
+    public sealed override bool CanWrite
         => _baseStream.CanWrite;
 
     /// <inheritdoc/>
-    public override bool CanSeek
+    public sealed override bool CanSeek
         => _baseStream.CanSeek;
 
     /// <inheritdoc/>
-    public override bool CanTimeout
+    public sealed override bool CanTimeout
         => _baseStream.CanTimeout;
 
     /// <inheritdoc/>
-    public override void CopyTo(Stream destination, int bufferSize)
+    public sealed override void CopyTo(Stream destination, int bufferSize)
         => _baseStream.CopyTo(destination, bufferSize);
 
     /// <inheritdoc/>
-    public override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
+    public sealed override Task CopyToAsync(Stream destination, int bufferSize, CancellationToken cancellationToken)
         => _baseStream.CopyToAsync(destination, bufferSize, cancellationToken);
 
     /// <inheritdoc/>
-    protected override void Dispose(bool disposing)
+    protected sealed override void Dispose(bool disposing)
     {
         // Can't call _baseStream.Dispose(bool) as it's protected,
         // so instead if disposing is true we call _baseStream.Dispose,
@@ -67,93 +67,93 @@ public abstract class DelegatingStream
     }
 
     /// <inheritdoc/>
-    public override ValueTask DisposeAsync()
+    public sealed override ValueTask DisposeAsync()
         => _baseStream.DisposeAsync();
 
     /// <inheritdoc/>
-    public override int EndRead(IAsyncResult asyncResult)
+    public sealed override int EndRead(IAsyncResult asyncResult)
         => _baseStream.EndRead(asyncResult);
 
     /// <inheritdoc/>
-    public override void EndWrite(IAsyncResult asyncResult)
+    public sealed override void EndWrite(IAsyncResult asyncResult)
         => _baseStream.EndWrite(asyncResult);
 
     /// <inheritdoc/>
-    public override void Flush()
+    public sealed override void Flush()
         => _baseStream.Flush();
 
     /// <inheritdoc/>
-    public override Task FlushAsync(CancellationToken cancellationToken)
+    public sealed override Task FlushAsync(CancellationToken cancellationToken)
         => _baseStream.FlushAsync(cancellationToken);
 
     /// <inheritdoc/>
-    public override long Length
+    public sealed override long Length
         => _baseStream.Length;
 
     /// <inheritdoc/>
-    public override long Position
+    public sealed override long Position
     {
         get => _baseStream.Position;
         set => _baseStream.Position = value;
     }
 
     /// <inheritdoc/>
-    public override int Read(byte[] buffer, int offset, int count)
+    public sealed override int Read(byte[] buffer, int offset, int count)
         => _baseStream.Read(buffer, offset, count);
 
     /// <inheritdoc/>
-    public override int Read(Span<byte> buffer)
+    public sealed override int Read(Span<byte> buffer)
         => _baseStream.Read(buffer);
 
     /// <inheritdoc/>
-    public override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    public sealed override Task<int> ReadAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         => _baseStream.ReadAsync(buffer, offset, count, cancellationToken);
 
     /// <inheritdoc/>
-    public override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
+    public sealed override ValueTask<int> ReadAsync(Memory<byte> buffer, CancellationToken cancellationToken = default)
         => _baseStream.ReadAsync(buffer, cancellationToken);
 
     /// <inheritdoc/>
-    public override int ReadByte()
+    public sealed override int ReadByte()
         => _baseStream.ReadByte();
 
     /// <inheritdoc/>
-    public override int ReadTimeout
+    public sealed override int ReadTimeout
     {
         get => _baseStream.ReadTimeout;
         set => _baseStream.ReadTimeout = value;
     }
 
     /// <inheritdoc/>
-    public override long Seek(long offset, SeekOrigin origin)
+    public sealed override long Seek(long offset, SeekOrigin origin)
         => _baseStream.Seek(offset, origin);
 
     /// <inheritdoc/>
-    public override void SetLength(long value)
+    public sealed override void SetLength(long value)
         => _baseStream.SetLength(value);
 
     /// <inheritdoc/>
-    public override void Write(byte[] buffer, int offset, int count)
+    public sealed override void Write(byte[] buffer, int offset, int count)
         => _baseStream.Write(buffer, offset, count);
 
     /// <inheritdoc/>
-    public override void Write(ReadOnlySpan<byte> buffer)
+    public sealed override void Write(ReadOnlySpan<byte> buffer)
         => _baseStream.Write(buffer);
 
     /// <inheritdoc/>
-    public override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
+    public sealed override Task WriteAsync(byte[] buffer, int offset, int count, CancellationToken cancellationToken)
         => _baseStream.WriteAsync(buffer, offset, count, cancellationToken);
 
     /// <inheritdoc/>
-    public override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
+    public sealed override ValueTask WriteAsync(ReadOnlyMemory<byte> buffer, CancellationToken cancellationToken = default)
         => _baseStream.WriteAsync(buffer, cancellationToken);
 
     /// <inheritdoc/>
-    public override void WriteByte(byte value)
+    public sealed override void WriteByte(byte value)
         => _baseStream.WriteByte(value);
 
     /// <inheritdoc/>
-    public override int WriteTimeout
+    public sealed override int WriteTimeout
     {
         get => _baseStream.WriteTimeout;
         set => _baseStream.WriteTimeout = value;
