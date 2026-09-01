ac229874d7ba2d3de2b94b4dcebe73466b76bb1a
Aleksander Heintz <alxandr@alxandr.me>
2026-09-01 16:45:42 +0200

    feat!: make repository loader internal

---
+++ src/pkgs/Altinn.Authorization.RepoCtl/src/RepoCtl.Model/Microsoft.Extensions.DependencyInjection/RepoCtlServiceCollectionExtensions.cs
@@ -0,0 +1,24 @@
+using Altinn.Authorization.RepoCtl.Model;
+
+namespace Microsoft.Extensions.DependencyInjection;
+
+/// <summary>
+/// Extension methods for registering RepoCtl services.
+/// </summary>
+public static class RepoCtlServiceCollectionExtensions
+{
+    /// <param name="services">The service collection.</param>
+    extension(IServiceCollection services)
+    {
+        /// <summary>
+        /// Adds the RepoCtl services to the service collection.
+        /// </summary>
+        /// <returns>The service collection.</returns>
+        public IServiceCollection AddRepoCtlServices()
+        {
+            services.AddSingleton<IAltinnRepositoryLoader, AltinnRepositoryLoader>();
+
+            return services;
+        }
+    }
+}
